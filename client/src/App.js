6dc784d0794e2a4ec9a48396e1e1ab1c5382df86
Luciano0322 <62631200+Luciano0322@users.noreply.github.com>
2020-09-10 11:26:53 +0800

    0910-2

---
+++ client/src/App.js
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
+
+import { listLogEntries } from './API';
+
+const App = () => {
+  const [logEntries, setLogEntries] = useState([]);
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: 37.6,
+    longitude: -95.665,
+    zoom: 3
+  });
+
+  useEffect(() => {
+    (async () => {
+      const logEntries = await listLogEntries();
+      setLogEntries(logEntries);
+    })();
+  }, []);
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      mapStyle="mapbox://styles/lucianolee0322/ckeuqzr5g0f1p19mpct21vpjh"
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      onViewportChange={nextViewport => setViewport(nextViewport)}
+    >
+      {
+        logEntries.map(entry => {
+          <Marker
+            key={entry._id}
+            latitude={entry.latitude}
+            longitude={entry.longitude}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <div>{entry.title}</div>
+          </Marker>
+        })
+      }
+    </ReactMapGL>
+  );
+}
+
+export default App;
